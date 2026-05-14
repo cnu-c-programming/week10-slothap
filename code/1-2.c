5067c1276126f5d2af36c0d37cb99cc20523101e
slothap <ueun6906@gmail.com>
2026-05-14 23:52:40 +0900

    tnwjd

--- code/1-2.c
+++ code/1-2.c
@@ -20,6 +20,6 @@ int main(){
     printf("ihl: %u, size: %zu\n", s.ihl ,sizeof(s.ihl));
     printf("ttl: %u, size: %zu\n", s.ttl ,sizeof(s.ttl));
     printf("protocol: %u, size: %zu\n", s.protocol ,sizeof(s.protocol));
-    printf("data: %u, size: %zu\n", s.data ,sizeof(s.data));
+    printf("data: %s, size: %zu\n", s.data ,sizeof(s.data));
     return 0;
 }
